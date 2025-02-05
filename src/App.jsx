d29637036998ed979d0d63d52d861b90c75d9f91
Juler Anderson Hermitano Castro <juleranderson@gmail.com>
2025-02-05 18:59:32 -0500

    First version of Valentine's Day Proposal website

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "./App.css";
+import image from "./assets/image.jpeg";
+
+function App() {
+  const [noPosition, setNoPosition] = useState({ top: "50%", left: "50%" });
+
+  const moveNoButton = () => {
+    const newTop = Math.random() * 80 + "%";
+    const newLeft = Math.random() * 80 + "%";
+    console.log("Nueva posición:", { top: newTop, left: newLeft });
+    setNoPosition({ top: newTop, left: newLeft });
+  };
+
+  return (
+    <div className="container">
+      <h1>Would you like to be my Valentine? ❤️</h1>
+      <img src={image} alt="San Valentín" className="photo" />
+      <div className="buttons">
+        <button className="yes">Sí ❤️</button>
+        <button
+          className="no"
+          style={{ top: noPosition.top, left: noPosition.left }}
+          onMouseEnter={moveNoButton}
+        >
+          No 💔
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
