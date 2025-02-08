1a68b142288ced03ff57ad91f991985410a4430c
Juler Anderson Hermitano Castro <juleranderson@gmail.com>
2025-02-07 21:05:38 -0500

    Add fade-out animation to IntroScreen for smoother transition

--- src/components/IntroScreen.jsx
+++ src/components/IntroScreen.jsx
@@ -1,13 +1,21 @@
 import PropTypes from "prop-types";
 import "./IntroScreen.css";
+import { useState } from "react";
 
 const IntroScreen = ({ startValentineScreen }) => {
+  const [hide, setHide] = useState(false);
+
+  const handleClick = () => {
+    setHide(true);
+    setTimeout(startValentineScreen, 1000);
+  };
+
   return (
     <div className="intro">
-      <div className="letter">
+      <div className={`letter ${hide ? "hide" : ""}`}>
         <p>Tengo algo que pedirte... 💌</p>
         <p>¿Quieres saber qué es? 🤭</p>
-        <button className="yes" onClick={startValentineScreen}>Sí, dime! 💖</button>
+        <button className="yes" onClick={handleClick}>Sí, dime! 💖</button>
       </div>
     </div>
   );
